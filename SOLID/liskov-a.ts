ca51c290724a6a4a723dbfdf6a503d8dcece016a
Angel Manuel Goez Giraldo <amgoez@siju.local>
2022-09-20 14:41:19 -0500

    Liskiv priciple homework done

--- SOLID/liskov-a.ts
+++ SOLID/liskov-a.ts
@@ -1,27 +1,29 @@
-import { Tesla, Audi, Toyota, Honda } from './liskov-b';
+import { Tesla, Audi, Toyota, Honda, Vehicle } from './liskov-b';
 
-const printCarSeats = ( cars: (Tesla | Audi | Toyota | Honda)[] ) => {
-        
-    for (const car of cars) {
+const printCarSeats = ( cars: Vehicle[] ) => {
+    cars.forEach(car => {
+        console.log(car.constructor.name, car.getNumberOfSeats())
+    })
+    /*for (const car of cars) {
      
         if( car instanceof Tesla ) {
-            console.log( 'Tesla', car.getNumberOfTeslaSeats() )
+            console.log( 'Tesla', car.getNumberOfSeats() )
             continue;
         }
         if( car instanceof Audi ) {
-            console.log( 'Audi', car.getNumberOfAudiSeats() )
+            console.log( 'Audi', car.getNumberOfSeats() )
             continue;
         }
         if( car instanceof Toyota ) {
-            console.log( 'Toyota', car.getNumberOfToyotaSeats() )
+            console.log( 'Toyota', car.getNumberOfSeats() )
             continue;
         }
         if( car instanceof Honda ) {
-            console.log( 'Honda', car.getNumberOfHondaSeats() )
+            console.log( 'Honda', car.getNumberOfSeats() )
             continue;
         }         
 
-    }
+    }*/
 }
 
 const cars = [
