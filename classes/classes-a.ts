339c1358172f2aeb9659dc7269707b158009b501
Angel Manuel Goez Giraldo <amgoez@siju.local>
2022-09-20 09:43:42 -0500

    Classes Person, User and userSetting to apply Single Reponsability Principle

--- classes/classes-a.ts
+++ classes/classes-a.ts
@@ -13,4 +13,34 @@ class Person {
 
 }
 
-const newPeroson = new Person('Hola', "F", new Date())
+class User extends Person {
+    public lastAccess: Date
+    constructor(
+        public email: string, 
+        public role: string,
+        name: string,
+        gender: Gender,
+        birthdate: Date
+    ){
+        super(name, gender, birthdate)
+        this.lastAccess = new Date()
+    }
+}
+
+class UserSettings extends User {
+    constructor(
+            public woringDirectory : string,
+            public lastOpenFolder : string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthdate: Date
+        ){
+        super(email, role, name, gender, birthdate);
+    }
+}
+
+const userSettings = new UserSettings('/usr/home', '/home', 'angel.goez','ADMIN', 'Angel Manuel', 'M', new Date('1995-02-26'))
+//const newPeroson = new Person('Angel Manuel', "M", new Date('1995-02-26'))
+//const newUser = new User('angel.goez', 'ROLE', 'Angel Manuel Góez Giraldo', 'M', new Date('1995-02-26'));
