1095dae594d44977d52658c7ab8bb6f590288e14
Angel Manuel Goez Giraldo <amgoez@siju.local>
2022-09-22 13:36:05 -0500

    Applying the Liskov sustitution and dependecies

--- SOLID/dependency-1.ts
+++ SOLID/dependency-1.ts
@@ -1,9 +1,12 @@
 import { PostService } from './dependency-2';
+import { JsonDataBaseService, LocalDataBaseService } from './dependency-3';
 
+const start = async () => {
+    // const provider = new JsonDataBaseService()
+    const provider = new LocalDataBaseService()
+    const postService = new PostService(provider);
+    const posts = await postService.getPosts();
+    console.log({ posts })
+}
 
-const postService = new PostService();
-
-const posts = await postService.getPosts();
-
-console.log({ posts })
-
+start()
